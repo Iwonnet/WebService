0ed52a54c5204438850114611becc2f678bdf363
Łukasz Iwon <lukasziwon1@gmail.com>
2019-02-03 17:58:58 +0100

    test new branch

---
+++ src/pl/lukasziwon/TheTwitter.java
@@ -0,0 +1,19 @@
+package pl.lukasziwon;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class TheTwitter {
+	
+	private String text;
+	
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
